59c2ac75b843ed7e46d775ee79deffc1942d6dbe
Renner Costa <ozielrennerc.rodrigues@gmail.com>
2024-10-24 16:04:34 -0300

    chore: ajustando configuração de vercel

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-
+  app.enableCors({
+    origin: "*",
+    allowedHeaders: "*"
+  });
   await app.listen(3001);
 }
 bootstrap();
